b8b2a4939ae6cb7537c1ec45eda829f6418b5088
astunov <astunov@gmail.com>
2017-01-16 18:50:02 +0300

    added letter config & data

---
+++ src/config.js
@@ -0,0 +1,40 @@
+// Email settings
+
+/**
+ * @namespace
+ * @property {String}  task — task name, used in utm
+ * @property {String}  date — actual date, used in utm
+ * @property {Object}  source — list of email clients
+ * @property {Boolean}  source.* — email client, used in utm && some unics moves for each
+ * @property {String}  type —
+ * @property {String}  prodSrc —
+ * @property {String}  src —
+ * @property {String}  alias — task alias used in utm (rewrites @task)
+ * @property {String}  letterWidth — default value is 600px
+ */
+
+const CONFIG = {
+  task: 'em430',
+  date: '1611',
+  source: {
+    ems: {
+      use: true,
+      utm: 'fbs-eMS'
+    },
+    gr: {
+      use: false,
+      utm: 'fbs-gr360'
+    },
+    mt: {
+      use: false,
+      utm: 'email-MT'
+    }
+  },
+  type: '' || 'promo',
+  prodSrc: '',
+  src: this.prodSrc || './i/',
+  // alias: '',
+  letterMaxWidth: 600
+};
+
+module.exports = CONFIG;
